e4f0dff1cd4ea19e6abc18492fee7af20a60aa7d
jeremy-b <jeremy.bernard@zaclys.net>
2019-03-07 15:57:27 +0100

    building_area indicator has been coded

--- geoindicators/src/test/groovy/org/orbisgis/IndicatorsDemoTests.groovy
+++ geoindicators/src/test/groovy/org/orbisgis/IndicatorsDemoTests.groovy
@@ -24,4 +24,23 @@ class IndicatorsDemoTests {
         indicatorDemo.demoProcess.execute([inputA : h2GIS.getSpatialTable("spatial_table")])
         assertTrue(indicatorDemo.demoProcess.results.outputA.equals(["ID", "THE_GEOM"]))
     }
+
+    @Test
+    void calculateBuildingArea() {
+        def h2GIS = H2GIS.open([databaseName: './target/loadH2GIS'])
+        String sqlFilePath = System.getProperty("user.dir")+"/target/TestDatasetCreation.sql"
+        String sqlString = new File(sqlFilePath).text
+        h2GIS.execute(sqlString)
+
+        h2GIS.execute("DROP TABLE IF EXISTS tempo_build; CREATE TABLE tempo_build AS SELECT * FROM building_test WHERE id_build = 1 OR id_build = 2 OR id_build = 3")
+
+        IndicatorDemo  indicatorDemo =  new IndicatorDemo()
+
+        indicatorDemo.buildingArea.execute([inputA: "tempo_build", inputB: "building_area", inputC: "building_area", inputD: "the_geom", inputE: h2GIS])
+
+        def concat = ""
+        h2GIS.eachRow("SELECT * FROM BUILDING_AREA")
+                {row -> concat += "$row.BUILDING_AREA\n"}
+        assertEquals("156.0\n40.0\n100.0\n", concat)
+    }
 }
