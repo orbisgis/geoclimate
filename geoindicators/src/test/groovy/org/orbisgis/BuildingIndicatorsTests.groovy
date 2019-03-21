e6fbd74dc7f1020292cabfee8947ac6ba236c49c
jeremy-b <jeremy.bernard@zaclys.net>
2019-03-21 13:04:42 +0100

    Coded the buildingNeighborsProperties

--- geoindicators/src/test/groovy/org/orbisgis/BuildingIndicatorsTests.groovy
+++ geoindicators/src/test/groovy/org/orbisgis/BuildingIndicatorsTests.groovy
@@ -48,4 +48,32 @@ class BuildingIndicatorsTests {
                 assertEquals(0,  row.building_passive_volume_ratio)
         }
     }
+
+    @Test
+    void buildingNeighborsProperties() {
+        def h2GIS = H2GIS.open([databaseName: './target/buildingdb'])
+        String sqlString = new File(this.class.getResource("data_for_tests.sql").toURI()).text
+        h2GIS.execute(sqlString)
+
+        // Only the first 6 first created buildings are selected since any new created building may alter the results
+        h2GIS.execute("DROP TABLE IF EXISTS tempo_build, building_neighbors_properties; CREATE TABLE tempo_build AS SELECT * " +
+                "FROM building_test WHERE id_build < 7")
+
+        def  p =  Geoclimate.BuildingIndicators.buildingNeighborsProperties()
+        p.execute([inputBuildingTableName: "tempo_build", inputFields:["id_build", "the_geom"],
+                   operations:["building_contiguity","building_common_wall_fraction",
+                               "building_number_building_neighbor"],
+                   outputTableName : "building_neighbors_properties",datasource:h2GIS])
+        def concat = ["", "", ""]
+        h2GIS.eachRow("SELECT * FROM building_neighbors_properties WHERE id_build = 1 OR id_build = 5 ORDER BY id_build ASC"){
+            row ->
+                concat[0]+= "${row.building_contiguity.round(5)}\n"
+                concat[1]+= "${row.building_common_wall_fraction.round(5)}\n"
+                concat[2]+= "${row.building_number_building_neighbor}\n"
+
+        }
+        assertEquals("0.0\n${(50/552).round(5)}\n".toString(),concat[0].toString())
+        assertEquals("0.0\n${(10/46).round(5)}\n".toString(), concat[1].toString())
+        assertEquals("0\n1\n".toString(),  concat[2].toString())
+    }
 }
