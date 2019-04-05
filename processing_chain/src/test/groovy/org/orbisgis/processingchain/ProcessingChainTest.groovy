cd0220713eb5d0af6fd0c188b00b19715c433f7b
jeremy-b <jeremy.bernard@zaclys.net>
2019-04-05 13:05:10 +0200

    Added the input BDTopo file but remove them from the git (gitignore)

--- processing_chain/src/test/groovy/org/orbisgis/processingchain/ProcessingChainTest.groovy
+++ processing_chain/src/test/groovy/org/orbisgis/processingchain/ProcessingChainTest.groovy
@@ -11,19 +11,19 @@ class ProcessingChainTest {
     void runBDTopoProcessingChain(){
         H2GIS h2GIS = H2GIS.open("./target/processingchaindb")
 
-/*      h2GIS.load(new File(this.class.getResource("IRIS_GE.shp").toURI()).getAbsolutePath(),"IRIS_GE",true)
-        h2GIS.load(new File(this.class.getResource("BATI_INDIFFERENCIE.shp").toURI()).getAbsolutePath(),"BATI_INDIFFERENCIE",true)
-        h2GIS.load(new File(this.class.getResource("BATI_REMARQUABLE.shp").toURI()).getAbsolutePath(),"BATI_REMARQUABLE",true)
-        h2GIS.load(new File(this.class.getResource("ROUTES.shp").toURI()).getAbsolutePath(),"ROUTES",true)
-        h2GIS.load(new File(this.class.getResource("RAIL.shp").toURI()).getAbsolutePath(),"RAIL",true)
-        h2GIS.load(new File(this.class.getResource("HYDRO.shp").toURI()).getAbsolutePath(),"HYDRO",true)
-        h2GIS.load(new File(this.class.getResource("VEGETATION.shp").toURI()).getAbsolutePath(),"VEGETATION",true)*/
+        h2GIS.load(new File(this.class.getResource("geoclimate_bdtopo_data_test/IRIS_GE.geojson").toURI()).getAbsolutePath(),"IRIS_GE",true)
+        h2GIS.load(new File(this.class.getResource("geoclimate_bdtopo_data_test/BATI_INDIFFERENCIE.geojson").toURI()).getAbsolutePath(),"BATI_INDIFFERENCIE",true)
+        h2GIS.load(new File(this.class.getResource("geoclimate_bdtopo_data_test/BATI_REMARQUABLE.geojson").toURI()).getAbsolutePath(),"BATI_REMARQUABLE",true)
+        h2GIS.load(new File(this.class.getResource("geoclimate_bdtopo_data_test/ROUTE.geojson").toURI()).getAbsolutePath(),"ROUTE",true)
+        h2GIS.load(new File(this.class.getResource("geoclimate_bdtopo_data_test/TRONCON_VOIE_FERREE.geojson").toURI()).getAbsolutePath(),"TRONCON_VOIE_FERREE",true)
+        h2GIS.load(new File(this.class.getResource("geoclimate_bdtopo_data_test/SURFACE_EAU.geojson").toURI()).getAbsolutePath(),"SURFACE_EAU",true)
+        h2GIS.load(new File(this.class.getResource("geoclimate_bdtopo_data_test/ZONE_VEGETATION.geojson").toURI()).getAbsolutePath(),"ZONE_VEGETATION",true)
 
         ProcessMapper pm =  ProcessingChain.prepareBDTopo.createMapper()
-        pm.execute([datasource: h2GIS, distBuffer : 500, expand : 1000, idZone : "12313", tableIrisName: "IRIS_GE",
+        pm.execute([datasource: h2GIS, distBuffer : 500, expand : 1000, idZone : "56260", tableIrisName: "IRIS_GE",
                     tableBuildIndifName: "BATI_INDIFFERENCIE", tableBuildIndusName: "BATI_INDUSTRIEL",
-                    tableBuildRemarqName: "BATI_REMARQUABLE", tableRoadName: "ROUTES", tableRailName: "RAIL",
-                    tableHydroName: "HYDRO", tableVegetName: "VEGETATION"])
+                    tableBuildRemarqName: "BATI_REMARQUABLE", tableRoadName: "ROUTE", tableRailName: "TRONCON_VOIE_FERREE",
+                    tableHydroName: "SURFACE_EAU", tableVegetName: "ZONE_VEGETATION"])
 
     }
 
