fc303f218d43a1d539bdf66a41f2c138475b16be
jeremy-b <jeremy.bernard@zaclys.net>
2019-04-05 11:34:33 +0200

    Processing chain - 3 first processes coded and tested - wait for BDTopo dataset

--- processing_chain/src/main/groovy/org/orbisgis/processingchain/PrepareBDTopo.groovy
+++ processing_chain/src/main/groovy/org/orbisgis/processingchain/PrepareBDTopo.groovy
@@ -15,12 +15,23 @@ import org.orbisgis.processmanagerapi.IProcess
 public static ProcessMapper createMapper(){
     def abstractTablesInit = AbstractTablesInitialization.initParametersAbstract()
     def bdTopoInitTypes = BDTopoGISLayers.initTypes()
+    def importPreProcess = BDTopoGISLayers.importPreprocess()
 
     ProcessMapper mapper = new ProcessMapper()
     mapper.link(outputBuildingAbstractUseType : abstractTablesInit, buildingAbstractUseType : bdTopoInitTypes)
     mapper.link(outputRoadAbstractType : abstractTablesInit, roadAbstractType : bdTopoInitTypes)
     mapper.link(outputRailAbstractType : abstractTablesInit, railAbstractType : bdTopoInitTypes)
     mapper.link(outputVegetAbstractType : abstractTablesInit, vegetAbstractType : bdTopoInitTypes)
+
+    mapper.link(outputBuildingAbstractUseType : abstractTablesInit, building_abstract_use_type : importPreProcess)
+    mapper.link(outputRoadAbstractType : abstractTablesInit, road_abstract_type : importPreProcess)
+    mapper.link(outputRailAbstractType : abstractTablesInit, rail_abstract_type : importPreProcess)
+    mapper.link(outputVegetAbstractType : abstractTablesInit, veget_abstract_type : importPreProcess)
+    mapper.link(outputBuildingBDTopoUseType : bdTopoInitTypes, building_bd_topo_use_type : importPreProcess)
+    mapper.link(outputroadBDTopoType : bdTopoInitTypes, road_bd_topo_type : importPreProcess)
+    mapper.link(outputrailBDTopoType : bdTopoInitTypes, rail_bd_topo_type : importPreProcess)
+    mapper.link(outputvegetBDTopoType : bdTopoInitTypes, veget_bd_topo_type : importPreProcess)
+
 
     return mapper
 }
--- processing_chain/src/test/groovy/org/orbisgis/processingchain/ProcessingChainTest.groovy
+++ processing_chain/src/test/groovy/org/orbisgis/processingchain/ProcessingChainTest.groovy
@@ -10,8 +10,20 @@ class ProcessingChainTest {
     @Test
     void runBDTopoProcessingChain(){
         H2GIS h2GIS = H2GIS.open("./target/processingchaindb")
+
+/*      h2GIS.load(new File(this.class.getResource("IRIS_GE.shp").toURI()).getAbsolutePath(),"IRIS_GE",true)
+        h2GIS.load(new File(this.class.getResource("BATI_INDIFFERENCIE.shp").toURI()).getAbsolutePath(),"BATI_INDIFFERENCIE",true)
+        h2GIS.load(new File(this.class.getResource("BATI_REMARQUABLE.shp").toURI()).getAbsolutePath(),"BATI_REMARQUABLE",true)
+        h2GIS.load(new File(this.class.getResource("ROUTES.shp").toURI()).getAbsolutePath(),"ROUTES",true)
+        h2GIS.load(new File(this.class.getResource("RAIL.shp").toURI()).getAbsolutePath(),"RAIL",true)
+        h2GIS.load(new File(this.class.getResource("HYDRO.shp").toURI()).getAbsolutePath(),"HYDRO",true)
+        h2GIS.load(new File(this.class.getResource("VEGETATION.shp").toURI()).getAbsolutePath(),"VEGETATION",true)*/
+
         ProcessMapper pm =  ProcessingChain.prepareBDTopo.createMapper()
-        pm.execute([datasource: h2GIS])
+        pm.execute([datasource: h2GIS, distBuffer : 500, expand : 1000, idZone : "12313", tableIrisName: "IRIS_GE",
+                    tableBuildIndifName: "BATI_INDIFFERENCIE", tableBuildIndusName: "BATI_INDUSTRIEL",
+                    tableBuildRemarqName: "BATI_REMARQUABLE", tableRoadName: "ROUTES", tableRailName: "RAIL",
+                    tableHydroName: "HYDRO", tableVegetName: "VEGETATION"])
 
     }
 
