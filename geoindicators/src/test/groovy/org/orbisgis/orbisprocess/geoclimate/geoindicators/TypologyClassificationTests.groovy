722b678d9eaacd77a5ba0f5174df62f603d74bd2
ebocher <erwan.bocher@univ-ubs.fr>
2019-12-06 16:02:58 +0100

    Randomforest test

--- geoindicators/src/test/groovy/org/orbisgis/orbisprocess/geoclimate/geoindicators/TypologyClassificationTests.groovy
+++ geoindicators/src/test/groovy/org/orbisgis/orbisprocess/geoclimate/geoindicators/TypologyClassificationTests.groovy
@@ -3,6 +3,7 @@ package org.orbisgis.orbisprocess.geoclimate.geoindicators
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.orbisgis.orbisdata.datamanager.dataframe.DataFrame
 import org.orbisgis.orbisdata.datamanager.jdbc.h2gis.H2GIS
 
 import static org.junit.jupiter.api.Assertions.assertEquals
@@ -69,4 +70,66 @@ class TypologyClassificationTests {
                 }
         }
     }
+
+    @Test
+    void createRandomForestClassifTest() {
+        h2GIS.execute """
+                DROP TABLE IF EXISTS tempo_rsu_for_lcz;
+                CREATE TABLE tempo_rsu_for_lcz AS SELECT a.*, b.the_geom FROM rsu_test_for_lcz a LEFT JOIN rsu_test b
+                ON a.id_rsu = b.id_rsu;
+        """
+        // Informations about where to find the training dataset for the test
+        def tableTraining = "testRandomForest"
+        def urlToDownload = ""
+        def pathAndNameFile = System.getProperty("user.home") + "/geoclimate/"+ tableTraining
+
+        // Variable to model
+        def var2model = "I_TYPO"
+        // File path to save the model
+        def fileAndPath = "/tmp/model.txt"
+
+        // Columns useless for the classification
+        String[] colsToRemove = ["THE_GEOM", "CITY", "PK_BUILDIN", "PK2", "PK_USR", "PK",\
+                        "PK_BLOCK_Z", "ID", "I_INHAB", "U_INHAB", "U_HOUSE", "U_COL_HOU", "U_HOUSE_A",\
+                     "U_COL_HOU_", "I_H_ORIGIN", "I_H", "I_LEVELS", "I_FLOOR", \
+                     "I_VOL", "I_COMP_B", "I_COMP_N", "I_WALL_A", "I_PWALL_A",\
+                     "I_FREE_EXT", "B_VOL", "B_H_MEAN", "B_H_STD", "B_COMP_N",\
+                     "U_FLOOR", "U_COS", "U_COMP_NWM", "U_COMP_WME", "U_H_MEAN",\
+                     "U_H_STD", "U_PASSIV_V", "U_VOL", "U_VOL_MEAN", "U_BH_STD_M",\
+                     "U_BCOMP_NW", "U_BCOMP_WM", "U_BCOMP_ST", "U_FWALL_A"]
+
+        // If the training dataset is not already loaded in the database...
+        if (h2GIS.getTable(tableTraining) == null){
+            // Test if the training dataset file if already exists...
+            File file = new File(pathAndNameFile)
+            if (!file){
+                // If not, download it
+                InputStream inputStream = new URL(urlToDownload).openStream()
+                Files.copy(inputStream, Paths.get(pathAndNameFile),
+                        StandardCopyOption.REPLACE_EXISTING)
+            }
+            h2GIS.execute """DROP TABLE IF EXISTS $tableTraining; 
+                                    CALL SHPREAD('$pathAndNameFile', '$tableTraining'); 
+                                    ALTER TABLE $tableTraining DROP COLUMN ${colsToRemove.join(",")};"""
+        }
+
+        def  pmed =  Geoindicators.TypologyClassification.createRandomForestClassif()
+        assertTrue pmed.execute([trainingTableName: tableTraining, varToModel: var2model,
+                                 save: true, pathAndFileName: fileAndPath,
+                                 ntrees: 300, mtry: 7, rule: SplitRule.GINI, maxDepth: 100,
+                                 maxNodes: 300, nodeSize: 5, subsample: 0.25, datasource: h2GIS])
+
+        // Test that the model has been correctly calibrated (that it can be applied to the same dataset)
+        DataFrame df = DataFrame.of(h2GIS.getTable(tableTraining)).factorize(var2model).omitNullRows()
+        def model = pmed.results.RfModel
+        assertEquals 0.844, Accuracy.of(df.apply(var2model).toIntArray(), Validation.test(model, df)).round(3), 0.002
+
+        // Test that the model is well written in the file and can be used to recover the variable names for example
+        /*XStream xs = new XStream()
+        FileInputStream fs = new FileInputStream(fileAndPath)
+        RandomForest modelRead = xs.fromXML(fs)
+        assertEquals h2GIS.getTable(tableTraining).getColumns().keySet().minus(var2model).sort().join(","),
+                modelRead.formula.x(df).names().sort().join(",")*/
+    }
+
 }
