c46c097025507875ba5bb3c7de3fd7bc0984852d
ebocher <erwan.bocher@univ-ubs.fr>
2020-02-21 18:39:08 +0100

    Fix delete db

--- processingchain/src/main/groovy/org/orbisgis/orbisprocess/geoclimate/processingchain/Workflow.groovy
+++ processingchain/src/main/groovy/org/orbisgis/orbisprocess/geoclimate/processingchain/Workflow.groovy
@@ -159,8 +159,17 @@ def BDTOPO_V2() {
                 if(geoclimatedb){
                     def h2gis_path = geoclimatedb.get("path")
                     def delete_h2gis_db = geoclimatedb.get("delete")
-                    if(delete_h2gis_db && delete_h2gis_db in Boolean){
-                        delete_h2gis = delete_h2gis_db
+                    if(delete_h2gis_db==null){
+                        delete_h2gis = true
+                    }
+                    else if (delete_h2gis_db instanceof String){
+                        delete_h2gis = true
+                        if(delete_h2gis_db.equalsIgnoreCase("false")){
+                            delete_h2gis=false
+                        }
+                    }
+                    else if(delete_h2gis_db instanceof Boolean){
+                        delete_h2gis=delete_h2gis_db
                     }
                     if(h2gis_path) {
                         h2gis_properties = ["databaseName":h2gis_path, "user": "sa", "password": ""]
@@ -1568,8 +1577,17 @@ def OSM() {
                 if(geoclimatedb){
                     def h2gis_path = geoclimatedb.get("path")
                     def delete_h2gis_db = geoclimatedb.get("delete")
-                    if(delete_h2gis_db && delete_h2gis_db in Boolean){
-                        delete_h2gis = delete_h2gis_db
+                    if(delete_h2gis_db==null){
+                        delete_h2gis = true
+                    }
+                    else if (delete_h2gis_db instanceof String){
+                        delete_h2gis = true
+                        if(delete_h2gis_db.equalsIgnoreCase("false")){
+                            delete_h2gis=false
+                        }
+                    }
+                    else if(delete_h2gis_db instanceof Boolean){
+                        delete_h2gis=delete_h2gis_db
                     }
                     if(h2gis_path) {
                         h2gis_properties = ["databaseName":h2gis_path, "user": "sa", "password": ""]
