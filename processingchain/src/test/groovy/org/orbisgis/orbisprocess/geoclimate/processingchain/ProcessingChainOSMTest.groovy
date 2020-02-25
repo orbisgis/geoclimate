568092ac9ec2193bec6dbcae20fd617b95d4ed40
ebocher <erwan.bocher@univ-ubs.fr>
2020-02-25 16:52:04 +0100

    Disable test

--- processingchain/src/test/groovy/org/orbisgis/orbisprocess/geoclimate/processingchain/ProcessingChainOSMTest.groovy
+++ processingchain/src/test/groovy/org/orbisgis/orbisprocess/geoclimate/processingchain/ProcessingChainOSMTest.groovy
@@ -360,7 +360,7 @@ class ProcessingChainOSMTest extends ChainProcessAbstractTest {
         assertTrue(process.execute(configurationFile: createOSMConfigFile(osm_parmeters, directory)))
     }
 
-    //@Disabled
+    @Disabled
     @Test
     void testOSMTEB() {
         String directory ="./target/geoclimate_chain"
