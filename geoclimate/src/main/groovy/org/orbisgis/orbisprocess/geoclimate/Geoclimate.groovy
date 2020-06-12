309dd0ad0b9477e93a032385f6e9ae5b708eb537
Palominos Sylvain <sylvain.palominos@univ-ubs.fr>
2020-06-12 11:55:21 +0200

    Fix imports.

--- geoclimate/src/main/groovy/org/orbisgis/orbisprocess/geoclimate/Geoclimate.groovy
+++ geoclimate/src/main/groovy/org/orbisgis/orbisprocess/geoclimate/Geoclimate.groovy
@@ -1,14 +1,12 @@
 package org.orbisgis.orbisprocess.geoclimate
 
-import examples.TestAutoregister
-import org.junit.Test
+
+import org.orbisgis.orbisprocess.geoclimate.bdtopo_v2.BDTopo_V2 as Topo
 import org.orbisgis.orbisprocess.geoclimate.geoindicators.Geoindicators as GI
 import org.orbisgis.orbisprocess.geoclimate.osm.OSM as O
-import org.orbisgis.orbisprocess.geoclimate.bdtopo_v2.BDTopo_V2 as Topo
 import org.orbisgis.orbisprocess.geoclimate.processingchain.ProcessingChain as PC
 
 import static org.orbisgis.orbisdata.processmanager.process.GroovyProcessManager.load
-
 /**
  * Root access point to the Geoindicators processes.
  */
