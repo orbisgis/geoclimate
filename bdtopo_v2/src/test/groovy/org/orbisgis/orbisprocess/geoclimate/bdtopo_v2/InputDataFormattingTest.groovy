f929fa2d102a74561d6b85fe79dfe11b4e7310ab
gpetit <gwendall.ec-nantes.fr>
2020-07-03 14:52:45 +0200

    resolving bug in shapefile

--- bdtopo_v2/src/test/groovy/org/orbisgis/orbisprocess/geoclimate/bdtopo_v2/InputDataFormattingTest.groovy
+++ bdtopo_v2/src/test/groovy/org/orbisgis/orbisprocess/geoclimate/bdtopo_v2/InputDataFormattingTest.groovy
@@ -37,7 +37,6 @@ class InputDataFormattingTest {
         }
         // Load data files
         listFilesBDTopo.each{
-            println(it)
             h2GISDatabase.load(getClass().getResource("$dataFolderInseeCode/${it.toLowerCase()}.shp"), it, true)
         }
     }
@@ -134,15 +133,6 @@ class InputDataFormattingTest {
             assertNotEquals('', row.ID_ZONE)
         }
 
-        //Check value for  specific features
-        //TODO: to be fixed nb_level is wrong
-        /*def res =  h2GISDatabase.firstRow("select type,  nb_lev, height_wall, height_roof from ${tableName} where ID_SOURCE='BATIMENT0000000257021459'")
-        assertEquals("building", res.TYPE)
-        assertEquals(1, res.NB_LEV)
-        assertEquals(6, res.HEIGHT_WALL)
-        assertEquals(6, res.HEIGHT_ROOF)*/
-
-        
         // Specific cases
         // -------------------------------
         // ... with the building 'BREMAR0001' : HAUTEUR = 0 / TYPE = 'Bâtiment sportif'
