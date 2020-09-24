8c91678cc5b04a0007d0ba55770e347d37f0b52e
erenault <e.renault.ubs@gmail.com>
2020-09-24 15:54:31 +0200

    modifs test and process for geoindicators

--- geoindicators/src/test/groovy/org/orbisgis/orbisprocess/geoclimate/geoindicators/GenericIndicatorsTests.groovy
+++ geoindicators/src/test/groovy/org/orbisgis/orbisprocess/geoclimate/geoindicators/GenericIndicatorsTests.groovy
@@ -498,6 +498,14 @@ class GenericIndicatorsTests {
         def query = """DROP TABLE IF EXISTS $indicatorTableName;
                        CREATE TABLE $indicatorTableName AS SELECT $indicatorName, the_geom FROM building_test;"""
         h2GIS.execute(query)
+
+        def value1 = h2GIS.firstRow("SELECT height_wall FROM building_test")[indicatorName]
+        def value2 = h2GIS.firstRow("SELECT $indicatorName FROM $indicatorTableName")[indicatorName]
+        def value3 = h2GIS.firstRow("SELECT the_geom FROM $indicatorTableName")['the_geom'].toString()
+        assert value1==8F
+        assert value2==8F
+        assertEquals('POLYGON ((4 4, 10 4, 10 30, 4 30, 4 4))', value3)
+
         def zonalAreaProcess = Geoindicators.GenericIndicators.zonalArea()
         zonalAreaProcess.execute(
                 [indicatorTableName: indicatorTableName,
