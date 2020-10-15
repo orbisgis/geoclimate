7cad9e5d2d9b5056aa2799004bb24aeeed9d66b4
jeremy-b <jeremy.bernard@zaclys.net>
2020-10-15 12:07:14 +0200

    Modify the column name in the distribution tables

--- processingchain/src/main/groovy/org/orbisgis/orbisprocess/geoclimate/processingchain/GeoIndicatorsChain.groovy
+++ processingchain/src/main/groovy/org/orbisgis/orbisprocess/geoclimate/processingchain/GeoIndicatorsChain.groovy
@@ -1252,8 +1252,8 @@ IProcess computeAllGeoIndicators() {
                 queryCasewhen.keySet().each{ind ->
                     def querySum = ""
                     listTypos.each{typoCol ->
-                        queryCasewhen[ind] += """ SUM(CASE WHEN a.I_TYPO=$typoCol THEN b.$ind ELSE 0 END) AS VAL$typoCol,"""
-                        querySum = querySum + " COALESCE(b.VAL${typoCol}/(b.VAL${listTypos.join("+b.VAL")}), 0) AS VAL$typoCol, "
+                        queryCasewhen[ind] += """ SUM(CASE WHEN a.I_TYPO=$typoCol THEN b.$ind ELSE 0 END) AS TYPO_$typoCol,"""
+                        querySum = querySum + " COALESCE(b.TYPO_${typoCol}/(b.TYPO_${listTypos.join("+b.TYPO_")}), 0) AS TYPO_$typoCol, "
                     }
                     // Calculates the distribution per RSU
                     datasource.execute """  DROP TABLE IF EXISTS $distribNotPercent;
