ba4a57eee675c95d1de1713ea20f4f5cabd2aa79
ebocher <erwan.bocher@univ-ubs.fr>
2022-06-29 14:56:58 +0200

    Add unit test to compute the  population at multiscales

--- geoindicators/src/main/groovy/org/orbisgis/geoclimate/geoindicators/PopulationIndicators.groovy
+++ geoindicators/src/main/groovy/org/orbisgis/geoclimate/geoindicators/PopulationIndicators.groovy
@@ -19,33 +19,33 @@ import org.orbisgis.process.api.IProcess
 IProcess formatPopulationTable() {
     return create {
         title "Format the population table"
-        inputs populationTable: String, populationColumns:[],
-                zoneTable:"",
+        inputs populationTable: String, populationColumns: [],
+                zoneTable: "",
                 datasource: JdbcDataSource
         outputs populationTable: String
         run { populationTable, populationColumns, zoneTable, datasource ->
-
             def tablePopulation_tmp = postfix(populationTable)
-
-            if(zoneTable){
-            datasource.execute("""
+            if (zoneTable) {
+                datasource.execute("""
                 CREATE SPATIAL INDEX ON $populationTable(the_geom);
                 CREATE SPATIAL INDEX ON $zoneTable(the_geom);
                 DROP TABLE IF EXISTS $tablePopulation_tmp ;
                 CREATE TABLE $tablePopulation_tmp AS SELECT ROWNUM() as id_pop, ST_MAKEVALID(a.the_geom) as the_geom , 
                 ${populationColumns.join(",")} from $populationTable as a ,$zoneTable as b where a.the_geom && b.the_geom 
-                and st_intersects(a.the_geom, b.the_geom) ;
+                and st_intersects(a.the_geom, b.the_geom) ;                
+                DROP TABLE IF EXISTS $populationTable;
+                ALTER TABLE $tablePopulation_tmp rename to $populationTable;
                 """.toString())
-            }
-            else{
+            } else {
                 datasource.execute("""
                 DROP TABLE IF EXISTS $tablePopulation_tmp ;
                 CREATE TABLE $tablePopulation_tmp AS SELECT rownum() as id_pop, ST_MAKEVALID(the_geom) as the_geom , 
                 ${populationColumns.join(",")} from $populationTable ;
-                ALTER TABLE $tablePopulation_tmp rename to $tablePopulationName;
+                DROP TABLE IF EXISTS $populationTable;
+                ALTER TABLE $tablePopulation_tmp rename to $populationTable;
                 """.toString())
             }
-            [populationTable: tablePopulation_tmp]
+            [populationTable: populationTable]
         }
     }
 }
@@ -63,73 +63,87 @@ IProcess formatPopulationTable() {
 IProcess multiScalePopulation() {
     return create {
         title "Process to distribute a set of population values at multiscales"
-        inputs populationTable : String , populationColumns: [],
-                buildingTable: String, rsuTable:"", gridPopTable: "",
+        inputs populationTable: String, populationColumns: [],
+                buildingTable: String, rsuTable: "", gridPopTable: "",
                 datasource: JdbcDataSource
-        outputs buildingPopTable: String, buildingRsuPopTable: String, rsuPopTable: String, gridPopTable: String
+        outputs buildingTable: String, rsuTable: String, gridTable: String
         run { populationTable, populationColumns, buildingTable, rsuTable, gridTable, datasource ->
-            if(populationTable && populationColumns){
+            if (populationTable && populationColumns) {
                 def prefixName = "pop"
-                def buildingPopTable=""
-                def buildingRsuPopTable=""
-                def rsuPopTable=""
-                def gridPopTable=""
-                if(buildingTable){
+                if (buildingTable) {
                     IProcess process = Geoindicators.BuildingIndicators.buildingPopulation()
-                    if(process.execute(inputBuildingTableName: buildingTable,
-                            inputPopulationTableName:populationTable , inputPopulationColumns: populationColumns,
+                    if (process.execute(inputBuildingTableName: buildingTable,
+                            inputPopulationTableName: populationTable, inputPopulationColumns: populationColumns,
                             datasource: datasource)) {
-                        buildingPopTable = process.results.buildingTableName
-                        if( rsuTable){
-                            //Let's aggregate all population indicators at RSU scale from buildings
-                            def sum_popColumns = []
-                            def popColumns =[]
-                            populationColumns.each { col ->
-                                sum_popColumns << "sum($col) as $col"
-                                popColumns<<"b.${col}"
-                            }
-                            def buildingPopRSU_tmp = postfix("building_rsu_pop_tmp")
-                            def buildingPopRSU = postfix("building_rsu_pop")
-                            datasource.execute("""DROP TABLE IF EXISTS $buildingPopRSU_tmp, $buildingPopRSU;
-                            CREATE TABLE $buildingPopRSU_tmp AS SELECT ${sum_popColumns.join(",")}, ID_RSU FROM ${buildingPopTable} GROUP BY ID_RSU;
-                            CREATE INDEX ON $buildingPopRSU_tmp (ID_RSU);
-                            CREATE TABLE $buildingPopRSU AS SELECT a.*, ${popColumns.join(",")} from $rsuTable a  
-                            LEFT JOIN $buildingPopRSU_tmp  b on a.ID_RSU=b.ID_RSU;
-                            DROP TABLE IF EXISTS $buildingPopRSU_tmp;
-                            """.toString())
-                            buildingRsuPopTable = buildingPopRSU
-                        }
-                        if(gridTable){
-                            //Here the population from the building to a grid
-                            // Create the relations between grid cells and buildings
-                            IProcess createScalesRelationsGridBl = Geoindicators.SpatialUnits.spatialJoin()
-                            if (!createScalesRelationsGridBl([datasource              : datasource,
-                                                              sourceTable             : buildingPopTable,
-                                                              targetTable             : gridTable,
-                                                              idColumnTarget          : "id_grid",
-                                                              prefixName              : prefixName,
-                                                              nbRelations             : null])) {
-                                info "Cannot compute the scales relations between buildings and grid cells."
-                                return
-                            }
-                            def unweightedBuildingIndicators =[:]
+                        datasource.execute("""DROP TABLE IF EXISTS $buildingTable;
+                                ALTER TABLE ${process.results.buildingTableName} RENAME TO $buildingTable""".toString())
+                        if (rsuTable) {
+                            def unweightedBuildingIndicators = [:]
                             populationColumns.each { col ->
                                 unweightedBuildingIndicators.put(col, ["sum"])
                             }
                             def computeBuildingStats = Geoindicators.GenericIndicators.unweightedOperationFromLowerScale()
-                            if (computeBuildingStats([inputLowerScaleTableName: createScalesRelationsGridBl.results.outputTableName,
-                                                       inputUpperScaleTableName: gridTable,
-                                                       inputIdUp               : "id_grid",
-                                                       inputIdLow              : "id_grid",
-                                                       inputVarAndOperations   : unweightedBuildingIndicators,
-                                                       prefixName              : prefixName,
-                                                       datasource              : datasource])) {
-                                gridPopTable = computeBuildingStats.results.outputTableName
+                            if (computeBuildingStats([inputLowerScaleTableName: buildingTable,
+                                                      inputUpperScaleTableName: rsuTable,
+                                                      inputIdUp               : "id_rsu",
+                                                      inputIdLow              : "id_rsu",
+                                                      inputVarAndOperations   : unweightedBuildingIndicators,
+                                                      prefixName              : prefixName,
+                                                      datasource              : datasource])) {
+                                def rsu_pop_tmp = computeBuildingStats.results.outputTableName
+                                def rsu_pop_geom = postfix(rsuTable)
+                                def p = Geoindicators.DataUtils.joinTables()
+                                def tablesToJoin = [:]
+                                tablesToJoin.put(rsuTable, "id_rsu")
+                                tablesToJoin.put(rsu_pop_tmp, "id_rsu")
+                                if (p([
+                                        inputTableNamesWithId: tablesToJoin,
+                                        outputTableName      : rsu_pop_geom,
+                                        datasource           : datasource])) {
+                                    datasource.execute("""DROP TABLE IF EXISTS $rsuTable, $rsu_pop_tmp;
+                                    ALTER TABLE $rsu_pop_geom RENAME TO $rsuTable;
+                                    DROP TABLE IF EXISTS $rsu_pop_geom;""".toString())
+                                }
+
                             }
                         }
+                        if (gridTable) {
+                            def sum_popColumns = []
+                            def popColumns = []
+                            populationColumns.each { col ->
+                                sum_popColumns << "sum($col*area_intersects/area_building) as SUM_$col"
+                                popColumns << "b.${col}"
+                            }
+                            //Here the population from the building to a grid
+                            // Create the relations between grid cells and buildings
+                            datasource."$buildingTable".the_geom.createSpatialIndex()
+                            datasource."$gridTable".the_geom.createSpatialIndex()
+                            def buildingGrid_inter = postfix("building_grid_inter")
+                            datasource.execute("""
+                                DROP TABLE IF EXISTS $buildingGrid_inter;
+                                CREATE TABLE $buildingGrid_inter AS SELECT ST_AREA(ST_INTERSECTION(a.the_geom, b.the_geom)) 
+                                AS area_intersects, st_area(b.the_geom) as area_building, ${popColumns.join(",")},a.id_grid from $gridTable as a, $buildingTable as b
+                                where a.the_geom && b.the_geom and ST_Intersects(a.the_geom, b.the_geom);                                
+                            """.toString())
+
+                            //Compute the population by cells
+                            def buildingPopGrid_tmp = postfix("building_grid_pop_tmp")
+                            def buildingPopGrid = postfix("building_grid_pop")
+                            datasource.execute("""DROP TABLE IF EXISTS $buildingPopGrid_tmp, $buildingPopGrid;
+                                CREATE INDEX ON $buildingGrid_inter (ID_GRID);
+                                CREATE TABLE $buildingPopGrid_tmp AS SELECT ${sum_popColumns.join(",")}, ID_GRID FROM ${buildingGrid_inter} GROUP BY ID_GRID;
+                                CREATE INDEX ON $buildingPopGrid_tmp (ID_GRID);
+                                CREATE TABLE $buildingPopGrid AS SELECT a.*, b.* EXCEPT(ID_GRID) from $gridTable a  
+                                LEFT JOIN $buildingPopGrid_tmp  b on a.ID_GRID=b.ID_GRID;
+                                DROP TABLE IF EXISTS $buildingPopGrid_tmp, $gridTable;
+                                ALTER TABLE $buildingPopGrid RENAME TO $gridTable;
+                                DROP TABLE IF EXISTS $buildingPopGrid;
+                                """.toString())
+                        }
+
                     }
                 }
-                return   [buildingPopTable: buildingPopTable, buildingRsuPopTable: buildingRsuPopTable, rsuPopTable: rsuPopTable, gridPopTable:gridPopTable]
+                return [buildingTable: buildingTable, rsuTable: rsuTable, gridTable: gridTable]
 
             }
             warn "Please set a valid population table name and a list of population columns"
