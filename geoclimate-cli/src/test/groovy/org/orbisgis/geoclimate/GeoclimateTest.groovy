d3447c796ce4ba1227b62e046c1fdd356e7f2b5f
ebocher <erwan.bocher@cnrs.fr>
2024-01-12 16:41:15 +0100

    Prepare release 1.0.0

--- geoclimate-cli/src/test/groovy/org/orbisgis/geoclimate/GeoclimateTest.groovy
+++ geoclimate-cli/src/test/groovy/org/orbisgis/geoclimate/GeoclimateTest.groovy
@@ -62,7 +62,7 @@ class GeoclimateTest {
 
     @Test
     void propertiesTest() {
-        assert "1.0.0" == Geoclimate.version
+        assert "1.0.0-SNAPSHOT" == Geoclimate.version
         assert Pattern.compile("^\\d{4}-\\d{2}-\\d{2}").matcher(Geoclimate.build).matches()
     }
 
