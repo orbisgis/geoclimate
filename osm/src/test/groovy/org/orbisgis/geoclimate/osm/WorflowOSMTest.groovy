2926ffd3088c48d71fd08a4afb395754c0e4a236
ebocher <erwan.bocher@cnrs.fr>
2026-05-27 12:34:19 +0200

    Update OrbisData and H2GIS

--- osm/src/test/groovy/org/orbisgis/geoclimate/osm/WorflowOSMTest.groovy
+++ osm/src/test/groovy/org/orbisgis/geoclimate/osm/WorflowOSMTest.groovy
@@ -731,7 +731,7 @@ class WorflowOSMTest extends WorkflowAbstractTest {
         dirFile.delete()
         dirFile.mkdir()
         def location = "Redon"
-        location = [45.56494, 5.897149, 45.566479, 5.899258]
+        location = [50 , -5.8 , 50.2 , -5.6 ]
         //def nominatim = OSMTools.Utilities.getNominatimData("Redon")
         def grid_size = 100
         //location =[47.214976592711274,-1.6425595375815742,47.25814872718718,-1.5659501122281323]
